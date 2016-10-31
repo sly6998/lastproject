58f79c2acf3a0d6703397c8cc4d735c5e3f5377c
HanGyundTaek <akfrdmsvlqn@naver.com>
2016-10-31 21:15:25 +0900

    [controller] qna 댓글 삭제 액션 추가

---
+++ web_project/src/com/interior/qna/QnaReplyDeleteAction.java
@@ -0,0 +1,41 @@
+package com.interior.qna;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.interior.controller.Action;
+import com.interior.controller.ActionForward;
+import com.interior.noti.NotiDAO;
+
+public class QnaReplyDeleteAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		ActionForward forward = new ActionForward();
+		request.setCharacterEncoding("utf-8");
+		
+		
+ 
+		boolean result = false;
+		int num = Integer.parseInt(request.getParameter("QNA_REPLY_SEQ"));
+
+		QnaDAO qnadao = new QnaDAO();
+		result = qnadao.qnaReplydelete(num);
+
+		
+		
+		
+		
+		if (result == false) {
+			System.out.println("QnA게시판 댓글 삭제 시스템 에러");
+			return null;
+		}
+
+		System.out.println("QnA게시판 댓글 삭제 완료");
+
+		forward.setRedirect(true);
+		forward.setPath("./qna_list.html");
+		return forward;
+	}
+}
