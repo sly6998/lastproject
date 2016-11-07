f4cd01e3c87b027c3cb0375a103eb6505e1845ab
yunje <yunje@Jun>
2016-11-07 13:35:22 +0900

    REVIEW BEAN 생성 그리고 에러 해결중... 건드리지 말아주세요 ㅠㅠ

--- web_project/src/com/interior/review/ReviewBean.java
+++ web_project/src/com/interior/review/ReviewBean.java
@@ -11,7 +11,14 @@ public class ReviewBean {
 	private int REVIEW_READCOUNT;
 	private Date REVIEW_DATE;
 	private int REVIEW_NUM;
-	
+	private String REVIEW_REPLY_MEMBER_NAME;
+	private String REVIEW_REPLY_MEMBER_ID;
+	private String REVIEW_REPLY_CONTENT;
+	private Date REVIEW_REPLY_DATE;
+	private int REVIEW_REPLY_NUM;
+	private int REVIEW_REPLY_REF;
+	private int REVIEW_REPLY_SEQ;
+	private int REVIEW_REPLY_LEV;
 	
 	public String getREVIEW_MEMBER_ID() {
 		return REVIEW_MEMBER_ID;
@@ -55,5 +62,52 @@ public class ReviewBean {
 	public void setREVIEW_NUM(int rEVIEW_NUM) {
 		REVIEW_NUM = rEVIEW_NUM;
 	}
-
+	public String getREVIEW_REPLY_MEMBER_NAME() {
+		return REVIEW_REPLY_MEMBER_NAME;
+	}
+	public void setREVIEW_REPLY_MEMBER_NAME(String rEVIEW_REPLY_MEMBER_NAME) {
+		REVIEW_REPLY_MEMBER_NAME = rEVIEW_REPLY_MEMBER_NAME;
+	}
+	public String getREVIEW_REPLY_MEMBER_ID() {
+		return REVIEW_REPLY_MEMBER_ID;
+	}
+	public void setREVIEW_REPLY_MEMBER_ID(String rEVIEW_REPLY_MEMBER_ID) {
+		REVIEW_REPLY_MEMBER_ID = rEVIEW_REPLY_MEMBER_ID;
+	}
+	public String getREVIEW_REPLY_CONTENT() {
+		return REVIEW_REPLY_CONTENT;
+	}
+	public void setREVIEW_REPLY_CONTENT(String rEVIEW_REPLY_CONTENT) {
+		REVIEW_REPLY_CONTENT = rEVIEW_REPLY_CONTENT;
+	}
+	public Date getREVIEW_REPLY_DATE() {
+		return REVIEW_REPLY_DATE;
+	}
+	public void setREVIEW_REPLY_DATE(Date rEVIEW_REPLY_DATE) {
+		REVIEW_REPLY_DATE = rEVIEW_REPLY_DATE;
+	}
+	public int getREVIEW_REPLY_NUM() {
+		return REVIEW_REPLY_NUM;
+	}
+	public void setREVIEW_REPLY_NUM(int rEVIEW_REPLY_NUM) {
+		REVIEW_REPLY_NUM = rEVIEW_REPLY_NUM;
+	}
+	public int getREVIEW_REPLY_REF() {
+		return REVIEW_REPLY_REF;
+	}
+	public void setREVIEW_REPLY_REF(int rEVIEW_REPLY_REF) {
+		REVIEW_REPLY_REF = rEVIEW_REPLY_REF;
+	}
+	public int getREVIEW_REPLY_SEQ() {
+		return REVIEW_REPLY_SEQ;
+	}
+	public void setREVIEW_REPLY_SEQ(int rEVIEW_REPLY_SEQ) {
+		REVIEW_REPLY_SEQ = rEVIEW_REPLY_SEQ;
+	}
+	public int getREVIEW_REPLY_LEV() {
+		return REVIEW_REPLY_LEV;
+	}
+	public void setREVIEW_REPLY_LEV(int rEVIEW_REPLY_LEV) {
+		REVIEW_REPLY_LEV = rEVIEW_REPLY_LEV;
+	}
 }
